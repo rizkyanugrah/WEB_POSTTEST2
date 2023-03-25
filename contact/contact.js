89658f13ddfa1251b03e0f34cb6df20c617fee85
Rizky Anugrah <muhammadrizkyanugrah01@gmail.com>
2023-03-26 04:18:51 +0800

    solve bug contact

--- contact/contact.js
+++ contact/contact.js
@@ -1,32 +1,38 @@
 // SAVE DATA DARI FORM
-const submit = document.getElementById("kirim");
+const submit = document.getElementById("submit");
 submit.onclick = Save_data;
 
 function Save_data(e) {
   e.preventDefault();
+  if (!$("#agree_checkbox").prop("checked")) {
+    $("#agree_chk_error").show();
+  } else {
+    $("#agree_chk_error").hide();
+    const nama_lengkap = document.getElementById("nama_lengkap").value;
+    const email = document.getElementById("email").value;
+    const phone = document.getElementById("phone").value;
+    const jenis_ps = document.getElementById("jenis_ps").value;
+    const jenis_kelamin = $(
+      "input[type='radio'][name='jenis_kelamin']:checked"
+    ).val();
+    const pesan = document.getElementById("pesan").value;
 
-  const nama_lengkap = document.getElementById("nama_lengkap").value;
-  const email = document.getElementById("email").value;
-  const phone = document.getElementById("phone").value;
-  const jenis_ps = document.getElementById("jenis_ps").value;
-  const jenis_kelamin = document.getElementById("jenis_kelamin").value;
-  const pesan = document.getElementById("pesan").value;
+    // mengambil data users yang sudah ada di local storage
+    var form_data = JSON.parse(sessionStorage.getItem("form_data")) || [];
 
-  // mengambil data users yang sudah ada di local storage
-  var form_data = JSON.parse(sessionStorage.getItem("form_data")) || [];
+    // menambahkan data users baru ke dalam array
+    form_data.push({
+      nama_lengkap: nama_lengkap,
+      email: email,
+      phone: phone,
+      jenis_ps: jenis_ps,
+      jenis_kelamin: jenis_kelamin,
+      pesan: pesan,
+    });
 
-  // menambahkan data users baru ke dalam array
-  form_data.push({
-    nama_lengkap: nama_lengkap,
-    email: email,
-    phone: phone,
-    jenis_ps: jenis_ps,
-    jenis_kelamin: jenis_kelamin,
-    pesan: pesan,
-  });
-
-  // menyimpan data users ke dalam local storage
-  sessionStorage.setItem("form_data", JSON.stringify(form_data));
-  alert("Terima Kasih Telah Menghubungi!");
-  window.location.href = "output.html";
+    // menyimpan data users ke dalam local storage
+    sessionStorage.setItem("form_data", JSON.stringify(form_data));
+    alert("Terima Kasih Telah Menghubungi!");
+    window.location.href = "output.html";
+  }
 }
